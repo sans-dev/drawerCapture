b46e880124e8a9599e5f5dfd28c5eaf5c95a5337
ssander.homeoffice <sebastian.sander@uni-bonn.de>
2023-05-04 14:36:57 +0200

    add camera stream logic

--- src/widgets/LiveModeWindow.py
+++ src/widgets/LiveModeWindow.py
@@ -1,6 +1,8 @@
+import subprocess
 from PyQt6.QtWidgets import QWidget, QPushButton, QGridLayout, QLabel
-from PyQt6.QtCore import Qt, QObject, pyqtSignal
-
+from PyQt6.QtCore import Qt, QObject, pyqtSignal, QThread, QTimer
+from PyQt6.QtGui import QImage, QPixmap
+import cv2
 from widgets.SelectCameraListWidget import SelectCameraListWidget
 
 class LiveModeClosedSignal(QObject):
@@ -11,6 +13,9 @@ class LiveModeWindow(QWidget):
         # Set the calling window as the parent
         super().__init__()
         self.liveModeClosedSignal = LiveModeClosedSignal()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updateLivePreview)
+
         self.initUI()
 
     def initUI(self):
@@ -39,24 +44,106 @@ class LiveModeWindow(QWidget):
         self.livePreviewLabel.setEnabled(False)
         self.livePreviewLabel.hide()
 
+        # add a button to start the live preview
+        self.startLivePreviewButton = QPushButton("Start Live Preview")
+        self.startLivePreviewButton.clicked.connect(self.startLivePreview)
+        self.startLivePreviewButton.setEnabled(False)
+
+        # add the preview and the button into a grid layout in the same cell
+        self.livePreviewLayout = QGridLayout()
+        self.livePreviewLayout.addWidget(self.livePreviewLabel, 0, 0)
+        self.livePreviewLayout.addWidget(self.startLivePreviewButton, 0, 0, Qt.AlignmentFlag.AlignCenter)
+
+        # create a list widget to select the camera
         self.selectCameraListWidget = SelectCameraListWidget()
         self.selectCameraListWidget.selectedCameraChanged.signal.connect(self.showLivePreviewOnCameraSelect)
         self.selectCameraListWidget.hide()
 
         # Add the widgets to the layout
-        self.layout.addWidget(self.exitModeButton, 2, 0)
-        self.layout.addWidget(self.livePreviewLabel, 1, 0)
         self.layout.addWidget(self.selectCameraButton, 0, 0)
+        self.layout.addLayout(self.livePreviewLayout, 1, 0)
+        self.layout.addWidget(self.exitModeButton, 2, 0)
 
     def selectCamera(self):
         self.selectCameraListWidget.show()
 
     def showLivePreviewOnCameraSelect(self, cameraName):
         self.livePreviewLabel.setText(cameraName)
-        self.livePreviewLabel.show()
-        self.selectCameraListWidget.hide()
+        self.startLivePreviewButton.setEnabled(True)
     
     def exitLiveMode(self):
         self.hide()
         self.liveModeClosedSignal.signal.emit()
+    
+    def startLivePreview(self):
+        self.cameraStreamer = CameraStreamer(self.selectCameraListWidget.selectedCameraData)
+        self.cameraStreamer.streamRunningSignal.signal.connect(self.startTimer)
+        self.cameraStreamer.start()
+
+    def startTimer(self):
+        self.timer.start(1)
+
+    def updateLivePreview(self):
+        ret, frame = self.cameraStreamer.videoCapture.read()
+        if ret:
+            rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, ch = rgbImage.shape
+            bytesPerLine = ch * w            
+            qt_image = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
+            pixmap = QPixmap.fromImage(qt_image)
+            self.livePreviewLabel.setPixmap(pixmap)
+
+
+class CameraStreamer(QThread):
+    def __init__(self, cameraData):
+        super().__init__()
+        self.cameraName = cameraData.split('usb')[0]
+        self.cameraPort = f"usb{cameraData.split('usb')[-1]}"
+        self.videoCapture = None
+        self.streamRunningSignal = StreamRunningSignal()
+
+    def run(self):
+        print(self.cameraName)
+        print(self.cameraPort)
+
+        cmd = [
+            'gphoto2', 
+            '--capture-movie', 
+            '--stdout', 
+            f'--port={self.cameraPort}', 
+            '|',
+            'ffmpeg', 
+            '-i', 
+            '-', 
+            '-vcodec', 
+            'rawvideo', 
+            '-pix_fmt', 
+            'yuv720p', 
+            '-threads',
+            '0',
+            '-f',
+            'v4l2',
+            f'/dev/{self.cameraName[0]}'
+        ]
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        while True:
+            output = proc.stdout.readline()
+            if proc.poll() is not None:
+                print("Process ended")
+                print(output.strip())
+                break
+            if output:
+                print(output.strip())
+            else:
+                print("Stream running")
+                break
+        self.videoCapture = cv2.VideoCapture(f'/dev/{self.cameraName}')
+        print(self.videoCapture.isOpened())
+        self.streamRunningSignal.signal.emit()
+
+class StreamRunningSignal(QObject):
+    signal = pyqtSignal()
+
+
+
         
--- src/widgets/SelectCameraListWidget.py
+++ src/widgets/SelectCameraListWidget.py
@@ -1,6 +1,6 @@
 import subprocess
 from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QListWidget
-from PyQt6.QtCore import pyqtSignal, QObject
+from PyQt6.QtCore import pyqtSignal, QObject, QThread
 
 class SelectCameraSignal(QObject):
     signal = pyqtSignal(str)
@@ -8,24 +8,23 @@ class SelectCameraSignal(QObject):
 class SelectCameraListWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.cameraList = self.getCameraList()
+        self.cameraFetcher = CameraFetcher()
         self.initUI()
 
     def initUI(self):
         # apply style sheet
         self.cameraListWidget = QListWidget()
-        self.cameraListWidget.addItems(self.cameraList)
-        print(self.cameraList)
 
         self.confirmButton = QPushButton('Confirm')
         self.confirmButton.clicked.connect(self.confirmSelection)
+        self.confirmButton.setEnabled(False)
 
         self.exitButton = QPushButton('Exit')
         self.exitButton.clicked.connect(self.close)
 
         # add a refresh button to get the latest list of cameras
         self.refreshButton = QPushButton('Refresh')
-        self.refreshButton.clicked.connect(self.refreshCameraList)
+        self.refreshButton.clicked.connect(self.refreshButtonClicked)
 
         layout = QVBoxLayout()
         layout.addWidget(self.refreshButton)
@@ -35,34 +34,52 @@ class SelectCameraListWidget(QWidget):
         self.setLayout(layout)
 
         # add a wait for response widget while camera list is being fetched
-        
+
         # add a signal to emit when a camera is selected
         self.selectedCameraChanged = SelectCameraSignal()
 
     def confirmSelection(self):
         selected_item = self.cameraListWidget.currentItem()
         if selected_item is not None:
-            print('Selected item:', selected_item.text())
             self.selectedCameraChanged.signal.emit(selected_item.text())
-        else:
-            print('No item selected')
+            self.selectedCameraData  = self.cameraFetcher.getCameraData(selected_item.text())
         self.hide()
 
-    def getCameraList(self):
+    def refreshButtonClicked(self):
+        self.refreshButton.setEnabled(False)
+        self.confirmButton.setEnabled(False)
+        self.cameraFetcher.finished.connect(self.updateCameraList)
+        self.cameraFetcher.start()
+
+    def updateCameraList(self, cameras):
+        self.cameraListWidget.clear()
+        if not cameras[0] == 'No cameras found':
+            self.confirmButton.setEnabled(True)
+        for camera in cameras:
+            self.cameraListWidget.addItem(camera)
+        self.refreshButton.setEnabled(True)
+
+class CameraFetcher(QThread):
+    finished  = pyqtSignal(list)
+
+    def run(self):
         cmd = ['gphoto2', '--auto-detect']
         output = subprocess.run(cmd, capture_output=True)
         lines = output.stdout.decode('utf-8').split('\n')
         cameras = []
+        self.cameras_data = []
         for line in lines:
-            if line.startswith('usb:'):
-                cameras.append(line.split()[0])
+            if 'usb:' in line:
+                cameras.append(line.split('usb:')[0])
+                self.cameras_data.append(line)
         # check if any cameras were found
         if len(cameras) == 0:
             cameras.append('No cameras found')
-        return cameras
-    
-    def refreshCameraList(self):
-        self.cameraList = self.getCameraList()
-        self.cameraListWidget.clear()
-        self.cameraListWidget.addItems(self.cameraList)
-        print(self.cameraList)
+        self.finished.emit(cameras)
+
+    def getCameraData(self, camera):
+        for camera_data in self.cameras_data:
+            if camera in camera_data:
+                return camera_data
+        return None
+
