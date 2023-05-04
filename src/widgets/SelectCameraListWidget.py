00f72f46b77897d2b47e27ede6f4cc78564ecf69
ssander.homeoffice <sebastian.sander@uni-bonn.de>
2023-05-04 12:53:35 +0200

    add camera basic camera scraping

--- src/widgets/SelectCameraListWidget.py
+++ src/widgets/SelectCameraListWidget.py
@@ -1,3 +1,4 @@
+import subprocess
 from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QListWidget
 from PyQt6.QtCore import pyqtSignal, QObject
 
@@ -7,12 +8,14 @@ class SelectCameraSignal(QObject):
 class SelectCameraListWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.cameraList = self.getCameraList()
         self.initUI()
 
     def initUI(self):
         # apply style sheet
-        self.listWidget = QListWidget()
-        self.listWidget.addItems(['Item 0', 'Item 2', 'Item 3'])
+        self.cameraListWidget = QListWidget()
+        self.cameraListWidget.addItems(self.cameraList)
+        print(self.cameraList)
 
         self.confirmButton = QPushButton('Confirm')
         self.confirmButton.clicked.connect(self.confirmSelection)
@@ -20,22 +23,46 @@ class SelectCameraListWidget(QWidget):
         self.exitButton = QPushButton('Exit')
         self.exitButton.clicked.connect(self.close)
 
+        # add a refresh button to get the latest list of cameras
+        self.refreshButton = QPushButton('Refresh')
+        self.refreshButton.clicked.connect(self.refreshCameraList)
+
         layout = QVBoxLayout()
-        layout.addWidget(self.listWidget)
+        layout.addWidget(self.refreshButton)
+        layout.addWidget(self.cameraListWidget)
         layout.addWidget(self.confirmButton)
+        layout.addWidget(self.exitButton)
         self.setLayout(layout)
 
+        # add a wait for response widget while camera list is being fetched
+        
+        # add a signal to emit when a camera is selected
         self.selectedCameraChanged = SelectCameraSignal()
-        print(self.selectedCameraChanged.__dict__)
 
     def confirmSelection(self):
-        selected_item = self.listWidget.currentItem()
+        selected_item = self.cameraListWidget.currentItem()
         if selected_item is not None:
             print('Selected item:', selected_item.text())
             self.selectedCameraChanged.signal.emit(selected_item.text())
         else:
             print('No item selected')
-        self.close()
-
-    def close(self):
         self.hide()
+
+    def getCameraList(self):
+        cmd = ['gphoto2', '--auto-detect']
+        output = subprocess.run(cmd, capture_output=True)
+        lines = output.stdout.decode('utf-8').split('\n')
+        cameras = []
+        for line in lines:
+            if line.startswith('usb:'):
+                cameras.append(line.split()[0])
+        # check if any cameras were found
+        if len(cameras) == 0:
+            cameras.append('No cameras found')
+        return cameras
+    
+    def refreshCameraList(self):
+        self.cameraList = self.getCameraList()
+        self.cameraListWidget.clear()
+        self.cameraListWidget.addItems(self.cameraList)
+        print(self.cameraList)
