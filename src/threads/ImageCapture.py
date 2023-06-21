a9d3aa355ee71237f347ac63653157e708fb0a0c
ssander.homeoffice <sebastian.sander@uni-bonn.de>
2023-06-21 15:46:12 +0200

    add logging to image capturing

--- src/threads/ImageCapture.py
+++ src/threads/ImageCapture.py
@@ -1,5 +1,11 @@
+import logging
+import logging.config
+logging.config.fileConfig('configs/logging.conf', disable_existing_loggers=False)
+
 from PyQt6.QtCore import QProcess
 from threads import CameraThread
+
+logger = logging.getLogger(__name__)
 
 class ImageCapture(CameraThread):
     IMG_FORMATS = {
@@ -15,10 +21,9 @@ class ImageCapture(CameraThread):
         self.config['--image_name'] = ''
         self.config['--image_format'] = '.tiff'
         self.config['--image_quality'] = '0'
-        self.config['--debug'] = 'true'
 
     def run(self):
-        print("image capture started")
+        logger.info("running image capture thread")
         super()._stopGphoto2Slaves()
         self._captureImage()
 
@@ -30,22 +35,23 @@ class ImageCapture(CameraThread):
             self.proc.readyReadStandardOutput.connect(self.printStdOut)
             self.proc.finished.connect(self._procFinished)
 
+            logger.debug("starting image capture process")
             self.proc.start(self.cmd, self._buildKwargs())
             started = self.proc.waitForStarted()
 
             if not started:
-                print("image capture failed to start")
+                logger.warining("failed to start image capture process")
                 return
             self.proc.waitForFinished(-1)
         
     def quit(self):
         super()._stopGphoto2Slaves()
         super().quit()
-        print("image capture closed")
+        logger.info("quitting image capture thread")
 
     def setUpConfig(self, config: dict):
         for key, value in config.items():
             try:
                 self.config[key] = value
             except KeyError:
-                print(f"key {key} not found in config")
+                logger.error("key %s not found in config", key)
