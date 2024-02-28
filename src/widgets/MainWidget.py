d51514a1a56e4aa34a7f4d852c4491ceee89dbdb
Sebastian <sebastian.sander@uni-bonn.de>
2024-02-28 08:58:44 +0100

    Add main widget functionality to create and open projects

--- src/widgets/MainWidget.py
+++ src/widgets/MainWidget.py
@@ -27,3 +27,12 @@ class MainWidget(QWidget):
         # Add the widgets to the layout
         self.layout.addWidget(self.create_new_project_bt, 0, 0, 1, 1)
         self.layout.addWidget(self.open_project_bt, 0, 1, 1, 1)
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt6.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    main_widget = MainWidget()
+    main_widget.show()
+    sys.exit(app.exec())
