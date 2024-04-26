f18c04832233bce799968ccb82595be7c4b46851
Sebastian <sebastian.sander@uni-bonn.de>
2024-04-26 12:08:33 +0200

    fixes

--- src/utils/fetch_gbif.py
+++ src/utils/fetch_gbif.py
@@ -31,43 +31,20 @@ keys_to_delete = [
     "lastInterpreted",
     "issues",
 ]
-keys_to_delete = [
-    "nubKey",
-    "nameKey",
-    "taxonID",
-    "sourceTaxonKey",
-    "kingdom",
-    "phylum",
-    "kingdomKey",
-    "phylumKey",
-    "classKey",
-    "orderKey",
-    "familyKey",
-    "genusKey",
-    "speciesKey",
-    "datasetKey",
-    "constituentKey",
-    "parentKey",
-    "authorship",
-    "nameType",
-    "rank",
-    "origin",
-    "taxonomicStatus",
-    "nomenclaturalStatus",
-    "remarks",
-    "numDescendants",
-    "lastCrawled",
-    "lastInterpreted",
-    "issues",
-]
-
-orders_response = requests.get('https://api.gbif.org/v1/species/216/children?limit=50').json()
-orders_response = [order for order in orders_response['results'] if order['rank'] == "ORDER"]
-orders_pbar = tqdm(orders_response, position=0, leave=True)
-
-limit = 800
-
-species = []
+timeout = 5
+try:
+    orders_response = requests.get('https://api.gbif.org/v1/species/216/children?limit=50', timeout=timeout)
+    orders_response.raise_for_status()
+    orders_response = orders_response.json()
+    orders_response = [order for order in orders_response['results'] if order['rank'] == "ORDER"]
+    orders_pbar = tqdm(orders_response, position=0, leave=True)
+except requests.exceptions.HTTPError as errh:
+    print(f"HTTP Error while fetching Orders") 
+    print(errh.args[0])
+except KeyError as e:
+    print(e)
+except Exception as e:
+    print(e)
 
 limit = 800
 
@@ -75,7 +52,7 @@ species = []
 for order in orders_pbar:
     order_name = order['order']
     try:
-        families_response = requests.get(f'https://api.gbif.org/v1/species/{order["key"]}/children?limit={limit}')
+        families_response = requests.get(f'https://api.gbif.org/v1/species/{order["key"]}/children?limit={limit}', timeout=timeout)
         families_response.raise_for_status()
         families_response = families_response.json()
         families_response = [fam for fam in families_response['results'] if fam['rank'] == "FAMILY"]
@@ -86,10 +63,13 @@ for order in orders_pbar:
     except KeyError as e:
         print(e)
         continue
+    except Exception as e:
+        print(e)
+        continue
     for family in families_response:
         family_name = family['scientificName']
         try:
-            genera_response = requests.get(f'https://api.gbif.org/v1/species/{family["key"]}/children?limit={limit}')
+            genera_response = requests.get(f'https://api.gbif.org/v1/species/{family["key"]}/children?limit={limit}', timeout=timeout)
             genera_response.raise_for_status()
             genera_response = genera_response.json()
             genera_response = [genus for genus in genera_response['results'] if genus['rank'] == "GENUS"]
@@ -100,11 +80,14 @@ for order in orders_pbar:
         except KeyError as e:
             print(e)
             continue
+        except Exception as e:
+            print(e)
+            continue
         for genus in genera_response:
             genus_name = genus['genus']
             orders_pbar.set_description(f"Fetching species for: {order_name}, {family['scientificName']}, {genus['genus']}...")
             try:
-                species_response = requests.get(f'https://api.gbif.org/v1/species/{genus["key"]}/children?limit={limit}', timeout=1)
+                species_response = requests.get(f'https://api.gbif.org/v1/species/{genus["key"]}/children?limit={limit}', timeout=timeout)
                 species_response.raise_for_status()
                 species_response = species_response.json()
                 species_response = [spec for spec in species_response['results'] if spec['rank'] == "SPECIES"]
@@ -113,6 +96,9 @@ for order in orders_pbar:
                 print(errh.args[0])
                 continue
             except KeyError as e:
+                print(e)
+                continue
+            except Exception as e:
                 print(e)
                 continue
 
