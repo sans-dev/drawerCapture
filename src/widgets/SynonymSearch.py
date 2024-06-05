15204d8e49d8ab9b5030d6293dd080022dbb36a1
Sebastian <sebastian.sander@uni-bonn.de>
2024-06-05 13:51:18 +0200

    add elements

--- src/widgets/SynonymSearch.py
+++ src/widgets/SynonymSearch.py
@@ -14,9 +14,30 @@ class SynonymSearch(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        # searchbar mit autocompletion
-        pass         
+        layout = QVBoxLayout()
+        label = QLabel("Region")
+        label.setFixedHeight(20)
+        self.region_input = QComboBox()
+        self.region_input.setInsertPolicy(QComboBox.InsertPolicy.NoInsert)
+        self.region_input.setFixedHeight(20)
+        region_edit = QLineEdit()
+        self.region_input.setLineEdit(region_edit)
+        # self.region_input.setEditable(True)
+        region_completer = QCompleter(self.regions['name'].to_list())
+        region_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        region_completer.setCompletionMode(
+            QCompleter.CompletionMode.PopupCompletion)
+        region_completer.setFilterMode(Qt.MatchFlag.MatchContains)
+        region_completer.setModelSorting(
+            QCompleter.ModelSorting.CaseInsensitivelySortedModel)
 
+        self.region_input.addItems(self.regions['name'])
+        self.region_input.setCompleter(region_completer)
+        self.region_input.currentIndexChanged.connect(self.on_region_changed)
+        layout.addWidget(label)
+        layout.addWidget(self.region_input)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.setLayout(layout)
 
 
 if __name__ == "__main__":
