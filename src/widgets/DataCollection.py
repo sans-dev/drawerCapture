cf66e02d558aaf3dd86f5b77273dd5bc1efb6ff1
Sebastian <sebastian.sander@uni-bonn.de>
2024-06-07 08:06:22 +0200

    add synonym search

--- src/widgets/DataCollection.py
+++ src/widgets/DataCollection.py
@@ -313,6 +313,113 @@ class GeoCoordinatesField(QWidget):
         self.longitude_input.setValue(coords[0])
         self.lattitude_input.setValue(coords[1])
 
+class SynonymSearch(QWidget):
+    name_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.name_str = None
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+        label = QLabel("Synonym Search")
+        label.setFixedHeight(20)
+        result_label = QLabel("Accepted Name")
+        self.accept_button = QPushButton("Accept")
+        self.close_button = QPushButton("Close")
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.accept_button)
+        button_layout.addWidget(self.close_button)
+        self.syn_input = QComboBox()
+        self.syn_input.setInsertPolicy(QComboBox.InsertPolicy.NoInsert)
+        self.syn_input.setFixedHeight(20)
+        syn_edit = QLineEdit()
+        self.syn_input.setLineEdit(syn_edit)
+
+        self.name = NonClickableListWidget()
+        self.name.setFixedHeight(20)
+
+        layout.addWidget(label)
+        layout.addWidget(self.syn_input)
+        layout.addWidget(result_label)
+        layout.addWidget(self.name)
+        layout.addLayout(button_layout)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.setLayout(layout)
+        self.syn_input.currentTextChanged.connect(self.on_syn_changed)
+
+        self.close_button.clicked.connect(self.close)
+        self.accept_button.clicked.connect(self.accept_name)
+
+    def accept_name(self):
+        self.name_signal.emit(self.name_str)
+
+    def on_syn_changed(self, synonyme):
+        self.name.clear()
+        try:
+            name = self.synonymes[synonyme]
+            self.name_str = name
+            self.name.addItem(QListWidgetItem(str(name)))
+        except KeyError:
+            pass
+    
+    def load_synonym_data(self, synonym_dir):
+        self.synonymes = json.load(
+            open(synonym_dir))
+        
+    def populate_ui(self):
+        synonyme_names = list(self.synonymes.keys())
+        synonyme_names.sort()
+        syn_completer = QCompleter(synonyme_names)
+        syn_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        syn_completer.setCompletionMode(
+            QCompleter.CompletionMode.InlineCompletion)
+        syn_completer.setFilterMode(Qt.MatchFlag.MatchStartsWith)
+        syn_completer.setModelSorting(
+            QCompleter.ModelSorting.CaseInsensitivelySortedModel)
+
+        self.syn_input.addItems(synonyme_names)
+        self.syn_input.setCompleter(syn_completer)
+        self.on_syn_changed(self.syn_input.currentText())
+        self.setLayout(layout)
+        self.syn_input.currentTextChanged.connect(self.on_syn_changed)
+
+        self.close_button.clicked.connect(self.close)
+        self.accept_button.clicked.connect(self.accept_name)
+
+    def accept_name(self):
+        self.name_signal.emit(self.name_str)
+
+    def on_syn_changed(self, synonyme):
+        self.name.clear()
+        try:
+            name = self.synonymes[synonyme]
+            self.name_str = name
+            self.name.addItem(QListWidgetItem(str(name)))
+        except KeyError:
+            pass
+    
+    def load_synonym_data(self, synonym_dir):
+        self.synonymes = json.load(
+            open(synonym_dir))
+        
+    def populate_ui(self):
+        synonyme_names = list(self.synonymes.keys())
+        synonyme_names.sort()
+        syn_completer = QCompleter(synonyme_names)
+        syn_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        syn_completer.setCompletionMode(
+            QCompleter.CompletionMode.InlineCompletion)
+        syn_completer.setFilterMode(Qt.MatchFlag.MatchStartsWith)
+        syn_completer.setModelSorting(
+            QCompleter.ModelSorting.CaseInsensitivelySortedModel)
+
+        self.syn_input.addItems(synonyme_names)
+        self.syn_input.setCompleter(syn_completer)
+        self.on_syn_changed(self.syn_input.currentText())
+
+
 class TaxonomyField(SearchableItemListWidget):
     parents_signal = pyqtSignal(list)
     clear_child_signal = pyqtSignal()
